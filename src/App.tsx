bcf1094af041ba9c8477e83fe51ad31435d399a7
Zowie0122 <minjiayi22@gmail.com>
2022-08-02 22:45:05 +0900

    [+] set up Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function App() {
-  return <div>Issue Tracker APP</div>;
+  return (
+    <div>
+      Issue Tracker APP
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
 }
 
 export default App;

5f9d09eab01ede275c8f09aa4f694e821e8bb541
Zowie0122 <minjiayi22@gmail.com>
2022-08-11 01:03:27 +0900

    [~+] adjusted the basic CSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
+import "./style/index.css";
 import store from "./store";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 const theme = createTheme();
 
@@ -14,6 +16,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </Provider>
